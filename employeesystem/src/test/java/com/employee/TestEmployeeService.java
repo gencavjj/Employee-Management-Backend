272c6d9bb03d81db2d376516be9952824af3824c
Jeremy <gencavjj@gmail.com>
2019-04-02 10:42:58 -0400

    created first tests

---
+++ employeesystem/src/test/java/com/employee/TestEmployeeService.java
@@ -0,0 +1,71 @@
+package com.employee;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.validation.constraints.Null;
+import java.util.ArrayList;
+import java.util.List;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class TestEmployeeService {
+
+    private int employeeId;
+    private Employee employee;
+    private List<Employee> employees;
+
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    private EmployeeService employeeService;
+
+    @Before
+    public void init() {
+
+        employeeId = 1;
+        employee = new Employee();
+        employees = new ArrayList<>();
+
+        employees.add(employee);
+
+        employeeService = new EmployeeService(employeeRepository);
+
+    }
+
+    @Test
+    public void testFindEmployees() {
+
+        when(employeeRepository.findAll()).thenReturn(employees);
+
+        List<Employee> returnedEmployees = employeeService.findEmployees();
+
+        Assert.assertEquals("There was an error getting the employees", employees, returnedEmployees);
+
+    }
+
+    @Test
+    public void testFindEmployee() {
+
+        when(employeeRepository.findById(employeeId)).thenReturn(employee);
+
+        Assert.assertEquals("There was an error getting the employees", employee,employee);
+
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testFindEmployee_employeeDoesNotExist_runtimeExceptionIsThrown() {
+
+        when(employeeRepository.findById(employeeId)).thenReturn(null);
+
+        employeeService.findEmployee(employeeId);
+
+    }
+
+}
