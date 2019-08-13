6dd6c1247c9834b9f1b94a5781609aad98cef64d
Jeremy <gencavjj@gmail.com>
2019-08-12 21:01:49 -0400

    updated h2 schema and application properties to reflect SQL functionality

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO EMPLOYEE (FIRST_NAME, LAST_NAME, EMAIL_ADDRESS, TEXT) VALUES ('Dennis', 'Kalaygian', 'dennis@gmail.com', 'hello');
-INSERT INTO EMPLOYEE (FIRST_NAME, LAST_NAME, EMAIL_ADDRESS, TEXT) VALUES ('Jeremy', 'Gencavage', 'jeremy@gmail.com', 'yes');
-INSERT INTO EMPLOYEE (FIRST_NAME, LAST_NAME, EMAIL_ADDRESS, TEXT) VALUES ('Lynn', 'Johnson', 'lynn@gmail.com', 'no');
-INSERT INTO EMPLOYEE (FIRST_NAME, LAST_NAME, EMAIL_ADDRESS, TEXT) VALUES ('K', 'T', 'KT.gmail.com', 'maybe');
-
-INSERT INTO NOTE (EMPLOYEE_ID, TEXT) VALUES (1, 'I eat trash on the beach for fun.');
-INSERT INTO NOTE (EMPLOYEE_ID, TEXT) VALUES (2, 'I am lost in space.');
